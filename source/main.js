7bca9683e8991cdb6834408f9f67def208a33a92
Jordan Benner <jordanbenner@outlook.com>
2017-07-09 19:03:28 -0500

    Added new css files
    Old css rep

--- source/main.js
+++ source/main.js
@@ -7,6 +7,18 @@ $(window).scroll(function (event) {
   } else {
     $('.header-compact').removeClass('transparent');
   }
+
+
+  var h = $(window).height();
+  var offset1 = $('iframe.frame1').offset();
+  if (scrollPos > offset1.top + 400 || scrollPos < offset1.top - h) {
+    $('.code-cover.cover1 > div').css('display', 'flex');
+  }
+
+  var offset2 = $('iframe.frame2').offset();
+  if (scrollPos > offset2.top + 400 || scrollPos < offset2.top - h) {
+    $('.code-cover.cover2 > div').css('display', 'flex');
+  }
 });
 // header about me text//
 // document.addEventListener("DOMContentLoaded", function(){
@@ -18,10 +30,49 @@ $(window).scroll(function (event) {
 
   $(function(){
 		$(".intro-entry").typed({
-			strings: ["Love building things.", "Aviation Enthusiast."],
+			strings: ["I have a Deep love building things.", "Pilot, Aviation Enthusiast.", "Avid college football fan."],
 			typeSpeed: 0,
       loop: true,
       showCursor: true,
       cursorChar: "|",
 		});
 	});
+
+function close_sidebar () {
+  $('.sidebar').css('right', '-300px');
+  setTimeout(function () {
+    $('.sidebar-outer').css('display', 'none');
+    $('body').css('overflow', 'auto');
+  }, 500);
+}
+
+// sidebar//
+$('a.nav-trigger').click(function (event) {
+  event.preventDefault();
+  $('body').css('overflow', 'hidden');
+  $('.sidebar-outer').css('display', 'block');
+  setTimeout(function () {
+    $('.sidebar').css('right', 0);
+  });
+});
+
+$('a.nav-close').click(function (event) {
+  event.preventDefault();
+  close_sidebar();
+});
+// experience selector
+function show_slide (event, slide) {
+  event.preventDefault();
+  $('.slides .slide').css('display', 'none');
+  var p = $('.slide-nav ' + slide).position();
+  console.log(p);
+  $('.slide-nav .moving').css('left', p.left + 'px');
+  setTimeout(function () {
+    $('.slides '+ slide).css('display', 'block');
+  });
+}
+
+function show_code (event, cover) {
+  event.preventDefault();
+  $('.code-cover.' + cover + '> div').css('display', 'none');
+}
